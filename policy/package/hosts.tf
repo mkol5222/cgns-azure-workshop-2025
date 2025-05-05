ea5cc6bcf8533117c181cb59d3f1f9c3ec4f671d
Martin Koldovsky <mkoldov@gmail.com>
2025-05-05 13:51:29 +0000

    .

--- policy/package/hosts.tf
+++ policy/package/hosts.tf
@@ -1,5 +1,11 @@
 resource "checkpoint_management_host" "example" {
   name         = "New Host 1"
   ipv4_address = "192.0.2.1"
+  color        = "blue"
+}
+
+resource "checkpoint_management_host" "pankrac" {
+  name         = "pankrac1"
+  ipv4_address = "192.0.2.107"
   color        = "red"
 }
